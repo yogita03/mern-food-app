c8556bb74437a30dbb84b71b409f44906ae7da1b
yogita03 <patilyogita408@gmail.com>
2023-08-27 11:15:29 +0530

    Carousel added

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ export default function Card() {
           className="card mt-3"
           style={{ width: "18rem", maxHeight: "360px" }}
         >
-          <img src="..." className="card-img-top" alt="..." />
+          <img src="https://media.istockphoto.com/id/1358536397/photo/set-of-plates-of-food-isolated-on-a-white-background.jpg?s=2048x2048&w=is&k=20&c=QcngedN0-zc_5tBKmlyMBYTDu9kk0kExEjpGFvdKrzY=" className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">This is some important text</p>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Card from "./Card";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
-    <div>
+    <div >
       <div>
         <Navbar />
       </div>
-      <div>
+      <div><Carousel/></div>
+      <div className="m-3">
         <Card />
       </div>
       <div>
