bcf7ecdee5c8815e913749f312602dfd2b5ea395
yogita03 <patilyogita408@gmail.com>
2023-08-27 09:14:10 +0530

    Navbar and card

---
+++ src/screens/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "./Card";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Card />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
