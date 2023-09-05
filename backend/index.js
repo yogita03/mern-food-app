ac5c81f73d3342a42b26e1782d0cb08aad23ce8e
yogita03 <patilyogita408@gmail.com>
2023-09-05 11:47:21 +0530

    Create SignUp Page and connect backend

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,21 @@ const port = 5000
 const mongoDB = require('./db')
 mongoDB();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with, Content-Type, Accept"
+  );
+  next();
 })
 
+
+app.use(express.json())
+app.use('/api',require("./Routes/CreateUser"));
+app.get('/', (req, res) => {
+  res.send('hello world...')
+})
 app.listen(port, ()=> {
     console.log(`Example app listening on port ${port}`)
 })
