998297b259a37a45470a9cece723350475488836
yogita03 <patilyogita408@gmail.com>
2023-09-08 10:57:52 +0530

    Login Page

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -30,4 +30,33 @@ res.json({success:true})
 }
 })
 
+
+router.post('/loginuser', [
+body('email').isEmail(),
+// password must be at least 5 chars long
+body('password','Incorrect Password').isLength({ min: 5 })],
+async(req,res) =>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    let email =  req.body.email
+    
+    try{
+   let userData =   await User.findOne({email})
+ if(!userData){
+    return res.status(400).json({ errors: "Try loggin with correct credentials" });
+ }
+
+ if(req.body.password !== userData.password){
+    return res.status(400).json({ errors: "Try loggin with correct credentials" });
+ }
+ 
+ return res.json({ success:true });
+    } catch(error){
+        console.log(error);
+        res.json({success:false})
+    }
+    })
+
 module.exports = router;
