9f37eabb928845dcae4db0a30dc2ad5fd322e081
yogita03 <patilyogita408@gmail.com>
2023-09-09 09:06:12 +0530

    Secure password using bcryptjs

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 
+const bcrypt = require('bcryptjs')
+
 
 router.post('/createuser', [
 body('email').isEmail(),
@@ -16,10 +18,13 @@ async(req,res) =>{
       return res.status(400).json({ errors: errors.array() });
     }
 
+    const salt = await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(req.body.password, salt)
+
 try{
  await User.create({
     name: req.body.name,
-    password: req.body.password,
+    password: secPassword,
     email: req.body.email,
     location: req.body.location
 })
